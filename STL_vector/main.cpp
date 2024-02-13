46f4ecee6d26cb4fe86d061209c9aa3f5e20b866
jxhe <jxhe@mail.ustc.edu.cn>
2024-02-13 10:52:20 +0800

    vector接口信息

--- STL_vector/main.cpp
+++ STL_vector/main.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main() {
 
 	vector<int> vi;
+	cout << "vi.size() = " << vi.size() << endl;
+	cout << "vi.capacity() = " << vi.capacity() << endl;
 	vi.push_back(1);
 	vi.push_back(2);
 	vi.push_back(3);
+	cout << "vi.size() = " << vi.size() << endl;
+	cout << "vi.capacity() = " << vi.capacity() << endl;
+	vi.push_back(4);
+	cout << "vi.capacity() = " << vi.capacity() << endl;
+
 	for (auto i = vi.begin(); i != vi.end(); i++) {
 		cout << *i << " ";
 	}
