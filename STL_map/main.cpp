965710f0cf172ad1d18197cfcb32525af2779598
jxhe <jxhe@mail.ustc.edu.cn>
2024-02-18 11:44:49 +0800

    map的赋值操作，遍历显示，下标表示法

--- STL_map/main.cpp
+++ STL_map/main.cpp
@@ -14,12 +14,25 @@ int main() {
 
 	mp.insert(pair<string, int>("hello", 100));
 	mp.insert(pair<string, int>("bob", 200));
+	mp["jim"] = 300;  // 下标赋值法
+	mp["greem"] = 400;
 
-	map<string, int>::iterator it = mp.begin();
-	for (; it != mp.end(); it++) {
+
+	for (map<string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
 		cout << it->first << " " << it->second << " " << endl;
 	}
 	cout << endl;
+
+	map<string, int> mp2;
+	mp2 = mp; // 将一个map对象赋值给另一个map对象，内容复制，mp的内容保持不变
+
+	mp["jack"] = 500;  // 修改mp的值，不改变mp2的内容，深度拷贝
+	
+	for (map<string, int>::iterator it = mp2.begin(); it != mp2.end(); it++) {
+		cout << it->first << " " << it->second << " " << endl;
+	}
+
+
 	
 
 }
