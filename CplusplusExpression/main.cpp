4efd7ada0bca919ddd60484caaaa2c07539679e0
jxhe <jxhe@mail.ustc.edu.cn>
2024-03-01 12:21:18 +0800

    表达式描述

--- CplusplusExpression/main.cpp
+++ CplusplusExpression/main.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include <string>
 using namespace std;
 void fun() {
 	cout << "fun()" << endl;
 }
 int main() {
+
+	string word;
+	int count = 0;
+	cin >> word;
+	while (word != "done" ) {
+		count++;
+		cin >> word;
+	}
+	cout << "You entered " << count << " words" << endl;
+
+	cout << """""""""""" << endl;
 	const char* p = "hello, world";
 	cout << (float*)p << endl;
 	cout << (int*)"hello, world" << endl;
