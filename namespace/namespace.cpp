c85c13c66433413de80ed2a5d8664710fcdfed2a
jxhe <jxhe@mail.ustc.edu.cn>
2024-03-06 22:31:33 +0800

    namespace用法

---
+++ namespace/namespace.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+namespace Jill {
+	int count = 9;
+	int number;
+}
+
+namespace Hill {
+	using Jill::count;
+	int color;
+	int height;
+	void show_count();
+}
+using namespace std;
+int main() {
+
+	cout << " count = " << Jill::count << endl;
+	cout << " count = " << Hill::count << endl;
+	using Hill::show_count;
+	show_count();
+	return 0;
+
+
+}
+
+namespace Hill{
+	void show_count() {
+		count++;
+		std::cout << " Hill::count = " << count << std::endl;
+	}
+}
