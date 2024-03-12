5b665fafead643f8b43f2365e9d432c2b887ec79
jxhe <jxhe@mail.ustc.edu.cn>
2024-03-12 21:50:48 +0800

    类内部声明结构

--- queue/queue.h
+++ queue/queue.h
@@ -3,15 +3,16 @@
 using std::cout;
 using std::endl;
 typedef int Item;
-typedef struct MyStruct
-{
-	Item item;
-	MyStruct* next;
-}PERSON;
+
 
 class Queue {
 	enum{Q_SIZE=10};
 private:
+	typedef struct MyStruct
+	{
+		Item item;
+		MyStruct* next;
+	}PERSON;
 	int qs;          // 最大队列长度
 	int length;       // 目前排队人数
 	PERSON* head;    // 队列头部
