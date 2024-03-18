c5794a04f7b02bbc66a435ff9c47f3976e2ba538
jxhe <jxhe@mail.ustc.edu.cn>
2024-03-18 23:47:29 +0800

    必须在子类构造函数的初始化列表处显示调用父类构造函数

--- inherit/tabtenn.cpp
+++ inherit/tabtenn.cpp
@@ -27,7 +27,11 @@ void TableTennisPlayer::info() const {
 	}
 }
 RelatedPlayer::RelatedPlayer(unsigned int r, const string p, const string& fn,
-	const string& ln, bool bt): TableTennisPlayer(fn, ln, bt), pd(p) {
+	const string& ln, bool bt): TableTennisPlayer(fn, ln, bt), pd(p) { 
+	// 子类构造函数不能访问父类私有变量，
+	// 通过在初始化列表方式显示调用父类构造函数，初始化属于父类的成员变量
+	// 还有一个逻辑上的原因是，只有对象以及构建，才能进入构造函数体内，而
+	// 子类对象是包含父类对象的全部属性，而此时还没调用父类的构造函数                                                    
 	
 	rating = r;
 }
