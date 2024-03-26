5916d1a6d1d692d5e3cf78d239e2830926cb92f8
jxhe <jxhe@mail.ustc.edu.cn>
2024-03-26 18:31:33 +0800

    transform STL非成员函数作用在容器对象上

--- genericProgramming/main.cpp
+++ genericProgramming/main.cpp
@@ -14,5 +14,13 @@ int main() {
 	if(pt != vd.end()){
 		cout << *pt << endl;
 	}
+	vector<double> vd2{ 6,5,4,3,2,1 };
+	vector<double> vd3(6);
+	// 确保三个vector对象的长度匹配
+	transform(vd.begin(), vd.end(), vd2.begin(), vd3.begin(), plus<double>());
+	for (auto& i : vd3) {
+		cout << i << " ";
+	}
+	cout << endl;
 	
 }
